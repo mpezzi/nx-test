15fb94d5dbdfda8954a26235e16c06d27cc4a5cd
Mike Pezzi <funkym@gmail.com>
2021-05-02 19:41:48 -0400

    added amplify yml

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/example-ng', '<rootDir>/libs/nx-test-ui'],
+  projects: [
+    '<rootDir>/apps/example-ng',
+    '<rootDir>/libs/nx-test-ui',
+    '<rootDir>/apps/example-ng-two',
+  ],
 };
